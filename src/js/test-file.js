6dd86d56daf145c121fe9abe8b8ded029017ed00
Mario Yepes <marioy47@gmail.com>
2020-07-06 17:57:12 -0500

    ESLint installation and configuration

---
+++ src/js/test-file.js
@@ -0,0 +1,15 @@
+// src/js/test-file.js
+
+var first_var;
+
+
+function aSimpleFunction()
+{ console.log('Hola mundo') }
+
+function anotherFunction(event) {
+      console.log('This one is used');
+}
+
+let other = document.getElementById('the-element-id')
+
+anotherFunction(null);
